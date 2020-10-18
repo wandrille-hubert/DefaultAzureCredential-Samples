e2efbdbbd2df09ae7ac8abf7af18beb462c59d41
Wandrille Hubert <wahube@microsoft.com>
2020-10-18 13:10:28 -0500

    update
    update

--- AzureGraphSample/Permissions.ps1
+++ AzureGraphSample/Permissions.ps1
@@ -1,4 +1,4 @@
-﻿Connect-AzureAD -TenantId dd218d36-09e8-4126-85e2-33eff0793b72
+﻿Connect-AzureAD -TenantId TENANTID
 $graph = Get-AzureADServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
 $groupReadPermission = $graph.AppRoles `
     | where Value -Like "Application.Read.All" `
