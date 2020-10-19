3b79b9d924839689611fddfb5c76d0b2106889b1
Ken Kilty <kkilty@microsoft.com>
2020-10-19 10:55:50 -0400

    Add prompts, docs ref to Permissions set ps1

--- AzureGraphSample/Permissions.ps1
+++ AzureGraphSample/Permissions.ps1
@@ -1,11 +1,14 @@
-﻿Connect-AzureAD -TenantId TENANTID
+﻿#Requires -PSEdition Desktop -Modules AzureAD
+Connect-AzureAD -TenantId $(Read-Host -Prompt "TenantId")
+# GraphAggregatorService = 00000003-0000-0000-c000-000000000000 the MS Graph's multi-tenant app
 $graph = Get-AzureADServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
 $groupReadPermission = $graph.AppRoles `
-    | where Value -Like "Application.Read.All" `
+    | Where-Object Value -Like "Application.Read.All" `
     | Select-Object -First 1
 
-$msi = Get-AzureADServicePrincipal -ObjectId MANAGEDIDENTITYOBJECTID
-
+$msi = Get-AzureADServicePrincipal -ObjectId $(Read-Host -Prompt "Managed Identity ObjectId")
+# https://docs.microsoft.com/en-us/azure/active-directory/users-groups-roles/directory-assign-admin-roles#privileged-role-administrator-permissions
+Write-Warning -Message "Note: Privileged Role Administrator required for any permission that requires admin consent."
 New-AzureADServiceAppRoleAssignment `
     -Id $groupReadPermission.Id `
     -ObjectId $msi.ObjectId `
